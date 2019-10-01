34fdd22fdaaa201d3583d840c6ba170e910d6894
loganfinnell <47046548+loganfinnell@users.noreply.github.com>
2019-09-30 18:16:32 -0700

    Update sketch.js

--- Assignments/CLOCKthatworks/clock/sketch.js
+++ Assignments/CLOCKthatworks/clock/sketch.js
@@ -1,18 +1,19 @@
 var gif_loadImg, gif_createImg;
 
 function preload() {
- 
+  gif_loadImg = loadImage("flower.gif");
   gif_createImg = createImg("flower.gif");
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(500, 700);
   background(0);
 }
 
 function draw() {
   // loads only first frame
- 
+  image(gif_loadImg, 50, 50);
+  
   // updates animation frames by using an html
   // img element, positioning it over top of
   // the canvas.
