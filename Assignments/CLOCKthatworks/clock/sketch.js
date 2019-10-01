f9032aa2d696e697935b55ccf8d07a389cbd4dce
loganfinnell <47046548+loganfinnell@users.noreply.github.com>
2019-09-30 18:11:22 -0700

    Update sketch.js

--- Assignments/CLOCKthatworks/clock/sketch.js
+++ Assignments/CLOCKthatworks/clock/sketch.js
@@ -1,19 +1,18 @@
 var gif_loadImg, gif_createImg;
 
 function preload() {
-  gif_loadImg = loadImage("https://creativecodingart2210fall2019section2.github.io/Finnell_Logan_ART2210/Assignments/CLOCKthatworks/clock/flower.gif");
-  gif_createImg = createImg("https://creativecodingart2210fall2019section2.github.io/Finnell_Logan_ART2210/Assignments/CLOCKthatworks/clock/flower.gif");
+ 
+  gif_createImg = createImg("flower.gif");
 }
 
 function setup() {
-  createCanvas(500, 700);
+  createCanvas(windowWidth, windowHeight);
   background(0);
 }
 
 function draw() {
   // loads only first frame
-  image(gif_loadImg, 50, 50);
-  
+ 
   // updates animation frames by using an html
   // img element, positioning it over top of
   // the canvas.
