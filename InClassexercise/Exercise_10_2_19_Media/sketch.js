cac72e69169c9b3f78d668731d9f147ce67ca9cd
loganfinnell <47046548+loganfinnell@users.noreply.github.com>
2019-10-02 14:28:23 -0500

    media

---
+++ InClassexercise/Exercise_10_2_19_Media/sketch.js
@@ -0,0 +1,18 @@
+function setup(){
+ 
+    createCanvas(windowWidth,windowHeight);
+  
+ }
+  
+ function draw(){// three-digit hexadecimal RGB notation
+    background('#fae');
+  
+ }
+ 
+  
+ function windowResized(){
+    resizeCanvas(windowWidth,windowHeight)
+ }
+ 
+
+ 
