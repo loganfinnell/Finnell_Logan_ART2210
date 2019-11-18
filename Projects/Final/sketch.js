c4a366b347a37c677e590dda8d4e9645e59334de
loganfinnell <47046548+loganfinnell@users.noreply.github.com>
2019-11-18 11:48:14 -0600

    Update sketch.js

--- Projects/Final/sketch.js
+++ Projects/Final/sketch.js
@@ -2,7 +2,7 @@
 
 //animations like p5 images should be stored in variables
 //in order to be displayed during the draw cycle
-var ghost, asterisk;
+var ghost;
 
 //it's advisable (but not necessary) to load the images in the preload function
 //of your sketch otherwise they may appear with a little delay
@@ -24,6 +24,6 @@ function draw() {
   //specify the animation instance and its x,y position
   //animation() will update the animation frame as well
   animation(ghost, 300, 150);
-  animation(asterisk, 500, 150);
+  
 }
 }
